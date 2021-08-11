817fba4d58f79862b7c580e904836a1a2e2fbb08
brunoecosta <brunoevcosta@gmail.com>
2021-08-11 16:17:36 -0300

    feign client

--- hr-payroll/src/main/java/com/devsuperior/hrpayroll/HrPayrollApplication.java
+++ hr-payroll/src/main/java/com/devsuperior/hrpayroll/HrPayrollApplication.java
@@ -1,9 +1,12 @@
 package com.devsuperior.hrpayroll;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class HrPayrollApplication {
 
 	public static void main(String[] args) {
