bf7fba2b3c57b5d365c99533da7ef1df8a5ab60c
brunoecosta <brunoevcosta@gmail.com>
2021-08-13 15:26:10 -0300

    configuring eureka clients and server

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+9
+	README.md,8/e/8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
+|
+Lhr-payroll/src/main/java/com/devsuperior/hrpayroll/HrPayrollApplication.java,8/b/8bd93206b06895f8189909d9500f61185205aaaf
--- hr-payroll/src/main/java/com/devsuperior/hrpayroll/HrPayrollApplication.java
+++ hr-payroll/src/main/java/com/devsuperior/hrpayroll/HrPayrollApplication.java
@@ -3,8 +3,11 @@ package com.devsuperior.hrpayroll;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableEurekaClient
 @SpringBootApplication
 @EnableFeignClients
 public class HrPayrollApplication {
