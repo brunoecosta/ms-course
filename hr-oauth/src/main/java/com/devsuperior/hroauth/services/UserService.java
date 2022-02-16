89826369b64dfdd8e19ae6092057c0e690261e2d
brunoecosta <brunoevcosta@gmail.com>
2022-02-16 00:29:27 -0300

    login - jwt token

--- hr-oauth/src/main/java/com/devsuperior/hroauth/services/UserService.java
+++ hr-oauth/src/main/java/com/devsuperior/hroauth/services/UserService.java
@@ -5,10 +5,13 @@ import com.devsuperior.hroauth.feingclients.UserFeignClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
 
     private static final Logger logger = LoggerFactory.getLogger(UserService.class);
 
@@ -26,4 +29,15 @@ public class UserService {
         return user;
     }
 
+    @Override
+    public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
+        User user = userFeignClient.findUserByEmail(userName).getBody();
+
+        if(null == user) {
+            logger.error("Email not found: " + userName);
+            throw new UsernameNotFoundException("Email not found:" + userName);
+        }
+        logger.info("Email found: " + userName);
+        return user;
+    }
 }
